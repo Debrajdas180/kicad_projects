3669528f57f612848dfc859baa6447bec286b12a
debraj <debraj@wooshair.com>
2023-07-05 16:33:45 +0530

    updated files

---
+++ docs/Jetson_Xavier_NX_Carrier_Board_Design_Files_A01/Jetson_Xavier_NX_Carrier_Board_Design_Files_A01/P3509_A01_Concept_schematic_design_files/P3509_A01_Concept_Schematic_design_files/temp/packagerxl.bat
@@ -0,0 +1,4 @@
+echo off
+netassembler
+concept2cm -forward -export -proj "C:\work\nvidia\Tegra\Xaiver_NX\customer_version\140-13509-0000-A01\p3509_a01_archive\p3509_a01.cpm"
+pxl.exe -proj "C:\work\nvidia\Tegra\Xaiver_NX\customer_version\140-13509-0000-A01\p3509_a01_archive\p3509_a01.cpm" -nosavehier
